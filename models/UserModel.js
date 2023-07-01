5db2cc9dc22717510377028a630c61cf02e01431
gihonhon <agunggihon@gmail.com>
2023-07-01 23:58:48 +0800

    schema example for users

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,1 +1,45 @@
-import express from 'express'
+import express from 'express';
+
+
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullname: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    img: {
+      type: String,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    balance: {
+      type: Number
+    },
+    orders: {
+      type: [String]
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
